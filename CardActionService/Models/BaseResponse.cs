3a0e738622a6c11605c2532754c7d43e8bb39b0a
Alexander Smushko <alexander.smushko@ticketvaultpos.com>
2025-03-09 20:31:39 +0000

    WebApi verifications added; request model created

---
+++ CardActionService/Models/BaseResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace CardActionService.Models
+{
+    public class BaseResponse<T>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+
+        public static BaseResponse<T> SuccessResponse(T data)
+        {
+            return new BaseResponse<T> { Success = true, Data = data };
+        }
+
+        public static BaseResponse<T> ErrorResponse(string message)
+        {
+            return new BaseResponse<T> { Success = false, Message = message };
+        }
+    }
+}
